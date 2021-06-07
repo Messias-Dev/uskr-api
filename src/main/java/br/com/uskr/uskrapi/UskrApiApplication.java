4c0eab36de12252c677eee690e8b0590840e10f3
Messias Nunesmaia <messias.lsn@gmail.com>
2021-06-07 20:55:15 -0300

    first commit

---
+++ src/main/java/br/com/uskr/uskrapi/UskrApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.uskr.uskrapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UskrApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UskrApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/uskr/uskrapi/UskrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uskr.uskrapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UskrApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
